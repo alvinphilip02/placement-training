d57158cc1becba345da5a04e60e817023bd046e7
alvinphilip02 <alvinphilip500@gmail.com>
2025-03-07 11:42:57 +0530

    Add files via upload

---
+++ tech.py
@@ -0,0 +1,33 @@
+"""count=5
+name='alvin'
+letter="A"
+avg=44.55
+is_married= False
+print(type(count))
+print(type(name))
+print(type(letter))
+print(type(avg))
+print(type(is_married))
+num1=float(input("enter num1: "))
+num2=float(input("enter num2: "))
+print(type(num1))
+print(type(num2))
+print(num1+num2)
+age=1
+if(age>=18 and age<=60):
+    print("welcome to the website!")
+    print("you have a great day!")
+elif(age<=18):
+    print("below age 18")
+else:
+    print("you are too smart!")
+    print("aa value porathada potta!")"""
+count =1
+while(count<=100):
+    print(count)
+    count +=1
+    if count==55:
+        count+=1
+        break
+            
+    
